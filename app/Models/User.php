e36d1bf6857a3538c6630136ff3dd6a2ee049e10
JP Caparas <jp@jpcaparas.com>
2017-08-05 10:39:04 +1200

    Add post relationship and soft deletes trait to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -26,4 +28,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Posts this user owns
+     *
+     * @return HasMany
+     */
+    public function posts()
+    {
+        $this->hasMany(Post::class);
+    }
 }
