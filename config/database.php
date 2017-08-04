6833cb4a1a01d46322b2fc9f82af918acdf2f17b
JP Caparas <jp@jpcaparas.com>
2017-08-05 07:44:42 +1200

    Add "Testing" DB connection for TDD
    Swap it over for MongoDB soon

--- config/database.php
+++ config/database.php
@@ -32,6 +32,11 @@ return [
     */
 
     'connections' => [
+        'testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
