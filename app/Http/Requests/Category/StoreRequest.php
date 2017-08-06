e8f816f94880c84cb060b4999e937b54033bb67f
JP Caparas <jp@jpcaparas.com>
2017-08-06 19:08:45 +1200

    Remove unique constraint from categories, as it is already slugified

--- app/Http/Requests/Category/StoreRequest.php
+++ app/Http/Requests/Category/StoreRequest.php
@@ -24,7 +24,7 @@ class StoreRequest extends ApiRequest
     public function rules()
     {
         return [
-            'name' => 'bail|required|max:255|unique:categories'
+            'name' => 'bail|required|max:255'
         ];
     }
 }
